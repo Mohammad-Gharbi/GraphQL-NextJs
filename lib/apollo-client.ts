cca76b55c0d0c900f8701c190a88ebd3f3d76ae9
Mohammed Gharbi <mohammedgharbi@icloud.com>
2025-02-01 13:05:04 +0100

    abstraction

--- lib/apollo-client.ts
+++ lib/apollo-client.ts
@@ -6,6 +6,6 @@ import {
 } from "@apollo/client"
 
 export const client = new ApolloClient({
-  uri: "https://flyby-router-demo.herokuapp.com/",
+  uri: "https://flyby-router-demo.herokuapp.com",
   cache: new InMemoryCache(),
 })
